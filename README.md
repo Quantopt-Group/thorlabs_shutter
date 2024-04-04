f84ee8173b591ea8e72244ff3c9ef3d3fbdda4c8
ausgebohmbt <karad@iesl.forth.gr>
2024-04-04 12:30:15 +0800

    works

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ python script to control the Thorlabs SC10 shutter controller
 
 once it works it can be turned into a widget to be able to be incorporated in the python gui
 
-to start I used code from:
+
+# end of the day it worked very straightforwardly by using pyVisa:
+https://pyvisa.readthedocs.io/en/latest/introduction/communication.html
+https://lampz.tugraz.at/~hadley/num/ch9/python/9.2.php
+
+I only implemented the initial query and turn on/off function though
+
+
+# initialy I had used code from the following links which I then deleted
 
 https://github.com/hebecked/PyDAQ/blob/master/bin/rotational_stage/old/instruments/thorlabs/sc10.py
 
@@ -11,11 +19,12 @@ and
 
 https://instrumentkit.readthedocs.io/en/latest/_modules/instruments/thorlabs/sc10.html
 
-# ackages required:
+# packages required:
 
 quantities:
 pip install quantities
 
 flufl.enum:
 pip install flufl.enum
+
 
---
+++ ma_thorShutr.py
@@ -0,0 +1,39 @@
+# import pyvisa
+#
+# rm = pyvisa.ResourceManager()
+# print(rm.list_resources())
+# inst = rm.open_resource('ASRL3::INSTR')
+# print(inst.query("id?\r"))
+import time
+
+import pyvisa as visa
+import numpy as np
+
+
+rm = visa.ResourceManager()
+instruments = np.array(rm.list_resources())
+print(instruments)
+
+my_instrument = rm.open_resource('ASRL3::INSTR')
+my_instrument.read_termination = '\r'
+my_instrument.write_termination = '\r'
+my_instrument.query('id?')
+try:
+    # my_instrument.write('id?')
+    identity = my_instrument.read()
+    print("Resource is" + str(my_instrument))
+    print("instrument is: " + identity + '\n')
+except visa.VisaIOError:
+    print('No connection to: ' + str(my_instrument))
+
+my_instrument.query('ens?')
+shut_state = my_instrument.read()
+print(shut_state)
+
+my_instrument.query('ens')
+# time.sleep(1)
+my_instrument.query('ens?')
+shut_state = my_instrument.read()
+print(shut_state)
+
+print("es el final")
